5b1faf176715b566c59325fb357efb269e52a673
mrCreese <99073893+mrCreese@users.noreply.github.com>
2023-03-25 11:37:34 +0100

    8kyu kata No Loops 2 - You only need one

---
+++ tasks/8kyu/No Loops 2 - You only need one/needOne.test.js
@@ -0,0 +1,38 @@
+const check = require('./NeedOne');
+
+const testExample = [
+    {
+        testCase: {
+            a: [66, 101],
+            x: 66,
+        },
+        result: true,
+    },
+    {
+        testCase: {
+            a: [80, 117, 115, 104, 45, 85, 112, 115],
+            x: 45,
+        },
+        result: true,
+    },
+    {
+        testCase: {
+            a: ['t', 'e', 's', 't'],
+            x: 'e',
+        },
+        result: true,
+    },
+    {
+        testCase: {
+            a: ['what', 'a', 'great', 'kata'],
+            x: 'kat',
+        },
+        result: false,
+    },
+];
+
+test('simple test', () => {
+    testExample.forEach((item) => {
+        expect(check(item.testCase.a, item.testCase.x)).toBe(item.result);
+    });
+});
