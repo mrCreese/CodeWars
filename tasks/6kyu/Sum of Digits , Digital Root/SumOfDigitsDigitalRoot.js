56157b0e2764e7f763b361adab8de3f35438e5a2
mrCreese <99073893+mrCreese@users.noreply.github.com>
2023-04-03 19:03:24 +0200

    newVersion

--- tasks/6kyu/Sum of Digits , Digital Root/SumOfDigitsDigitalRoot.js
+++ tasks/6kyu/Sum of Digits , Digital Root/SumOfDigitsDigitalRoot.js
@@ -1,11 +1,12 @@
 function digitalRoot(n) {
-    while (n > 9) {
-        n = n
+    while (n < 10) return n;
+
+    return digitalRoot(
+        n
             .toString()
             .split('')
-            .reduce((acc, cur) => acc + Number(cur), 0);
-    }
-    return n;
+            .reduce((acc, cur) => acc + Number(cur), 0)
+    );
 }
 
 module.exports = digitalRoot;
